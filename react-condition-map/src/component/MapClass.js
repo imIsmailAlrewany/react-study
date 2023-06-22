122d181775dabad353f1c464692848883d0b0417
imIsmailAlrewany <ismailalrewany332@gmail.com>
2023-06-22 10:10:05 +0200

    code stoic

--- react-condition-map/src/component/MapClass.js
+++ react-condition-map/src/component/MapClass.js
@@ -3,7 +3,15 @@ import { Component } from 'react';
 class MapClass extends Component {
   constructor() {
     super();
-    this.languagesList = ['HTML', 'CSS', 'JavaScript', 'Python', 'C++'];
+    // this.languagesList = ['HTML', 'CSS', 'JavaScript', 'Python', 'C++'];
+
+    this.languagesList = [
+      { id: 1, lang: 'HTML' },
+      { id: 2, lang: 'CSS' },
+      { id: 3, lang: 'JavaScript' },
+      { id: 4, lang: 'Python' },
+      { id: 5, lang: 'C++' },
+    ];
   }
 
   render() {
@@ -11,10 +19,26 @@ class MapClass extends Component {
     // return output;
 
     return (
+      // first way to create key
+      // <div>
+      //   <h3>Map from class component</h3>
+      //   {this.languagesList.map((language) => <p>{language}</p>)}
+      // </div >
+
+      // second way to create key
+      // <div>
+      //   <h3>Map from class component</h3>
+      //   {this.languagesList.map((language, index) => <p key={index}>{language}</p>)}
+      // </div>
+
+      // third way to create key
       <div>
         <h3>Map from class component</h3>
-        {this.languagesList.map((language) => <p>{language}</p>)}
-      </div >
+        {this.languagesList.map((language) => <p key={language.id}>{language.lang}</p>)}
+      </div>
+
+      // fourth way to create key is using plugin to create random ids
+
     );
   }
 }
--- react-condition-map/src/component/MapFunctional.js
+++ react-condition-map/src/component/MapFunctional.js
@@ -1,14 +1,36 @@
 function MapFunctional() {
-  const languagesList = ['HTML', 'CSS', 'JavaScript', 'Python', 'C++'];
+  // const languagesList = ['HTML', 'CSS', 'JavaScript', 'Python', 'C++'];
+  const languagesList = [
+    { id: 1, lang: 'HTML' },
+    { id: 2, lang: 'CSS' },
+    { id: 3, lang: 'JavaScript' },
+    { id: 4, lang: 'Python' },
+    { id: 5, lang: 'C++' },
+  ];
 
   // const output = languagesList.map((language) => <p>{language}</p>);
   // return output;
 
   return (
+    // first way to create key
+    // <div>
+    //   <h3>Map from function component</h3>
+    //   {languagesList.map((language) => <p>{language}</p>)}
+    // </div>
+
+    // second way to create key
+    // <div>
+    //   <h3>Map from function component</h3>
+    //   {languagesList.map((language, index) => <p key={index}>{language}</p>)}
+    // </div>
+
+    // third way to create key
     <div>
       <h3>Map from function component</h3>
-      {languagesList.map((language) => <p>{language}</p>)}
+      {languagesList.map((language) => <p key={language.id}>{language.lang}</p>)}
     </div>
+
+    // fourth way using plugin to create random unique ids
   );
 }
 
