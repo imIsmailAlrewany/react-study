f688bae43d16aa50bb40f6ba312ee4f48b2c990b
imIsmailAlrewany <ismailalrewany332@gmail.com>
2023-06-25 10:46:18 +0200

    code stoic

--- react-class-style/src/component/TempComponent.js
+++ react-class-style/src/component/TempComponent.js
@@ -1,8 +1,8 @@
-import styles from "./style.module.css";
+// import styles from "./style.module.css";
 
 export default function TempComponent() {
   return (
-    <h3 className="">
+    <h3 className="heading">
       This is from Css module Component using CSS Module
     </h3>
   );
